f65e2681eeaec7752d006b2674d82da96761ef8d
AlexandreCavanna <alexandre.cavanna.pro@gmail.com>
2021-08-22 19:12:54 +0200

    add api show user details for a customer

--- src/Controller/ApiUserController.php
+++ src/Controller/ApiUserController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Customer;
+use App\Entity\User;
 use App\Repository\CustomerRepository;
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,5 +19,20 @@ class ApiUserController extends AbstractController
     public function showUsersIndex(Customer $customer, CustomerRepository $customerRepository): Response
     {
         return $this->json($customerRepository->find($customer), 200, [], ['groups' => 'show_customer_users_index']);
+    }
+
+    /**
+     * @Route("/api/customer/{customerId}/user/{userId}", name="api_user_details")
+     * @ParamConverter("customer", options={"mapping": {"customerId": "id"}})
+     * @ParamConverter("user", options={"mapping": {"userId": "id"}})
+     */
+    public function showUserDetails(Customer $customer, User $user, UserRepository $userRepository): Response
+    {
+        return $this->json(
+            $userRepository->findOneBy(['customer' => $customer, 'id' => $user]),
+            200,
+            [],
+            ['groups' => 'show_customer_user_details']
+        );
     }
 }
